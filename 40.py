fcb73191bae13925b4ae550432a173b373037f6a
cg-dv <cgdevproject@gmail.com>
2020-04-23 18:29:35 -0400

    Added solution to problem 40

---
+++ 40.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+s = ''
+for i in range(1,1000000):
+    s = s + str(i)
+
+print(int(s[0]) * int(s[9]) * int(s[99]) * int(s[999]) * int(s[9999]) * int(s[99999]) * int(s[999999]))
